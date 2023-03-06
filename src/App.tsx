4cb22591ef4ebc461aebdf8a1e31940768697567
sulaman <s.sulamanshahbukhari@gmail.com>
2023-03-06 12:04:40 +0500

    voide type

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import TypeAnnoations from './BasicTypes/TypeAnnoations'
 import TypeInference from './BasicTypes/TypeInference'
 import UnderstandingAnyType from './BasicTypes/UnderstandingAnyType'
+import VoidType from './BasicTypes/VoidType'
 
 const App = () => {
   return (
     <>
     {/* <TypeAnnoations/> */}
     {/* <TypeInference/> */}
-    <UnderstandingAnyType/>
+    {/* <UnderstandingAnyType/> */}
+    <VoidType/>
 
 
     
--- src/BasicTypes/VoidType.tsx
+++ src/BasicTypes/VoidType.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
 const VoidType = () => {
+  function logMessage(message: string): void {
+    console.log(message);
+  }
+  logMessage("Welcome Sulaman");
   return (
-    <div>VoidType</div>
+    <>
+    <h3>Void type</h3>
+    <p>So, the void type is to define that a function doesn’t return anything. TypeScript will correctly infer this type if a function doesn’t return anything, so we don’t need to define it explicitly.</p>
+    <b>when to use Void type</b>
+    <p> void is only really useful for function return types and it can be explicitly defined on functions after the parameter parentheses like in the example above:
+    </p>
+    
+  
+    </>
   )
 }
 
